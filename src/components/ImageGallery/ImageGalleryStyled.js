47d4c725f36d30fb2de5f4d76d57dae106c11bf2
Ann Maksay <annaorlova.dn@gmail.com>
2021-06-08 17:05:47 +0300

    fix style

--- src/components/ImageGallery/ImageGalleryStyled.js
+++ src/components/ImageGallery/ImageGalleryStyled.js
@@ -4,7 +4,10 @@ const ImageGalleryWrapper = styled.div`
   .ImageGallery {
     display: grid;
     max-width: calc(100vw - 48px);
-    grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+    grid-template-columns: repeat(
+      auto-fill,
+      minmax(calc(100% / 4 - 16px * 3), 1fr)
+    );
     grid-gap: 16px;
     margin-top: 0;
     margin-bottom: 0;
